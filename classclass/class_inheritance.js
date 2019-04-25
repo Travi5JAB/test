e5c6b548892595b07b6327a1958bc3c89876e25d
Travi5JAB <travis841235@gmail.com>
2019-04-25 16:29:36 -0700

    initail commit

--- classclass/class_inheritance.js
+++ classclass/class_inheritance.js
@@ -6,14 +6,14 @@ class Car{
         this.signal = "OFF"
         this. speed = speed
     }
-    lightsOn(){
-        this.lights = "ON"
-        return this.lights
-    }
-    lightsOff(){
-        this.lights = "OFF"
-        return this.lights
-    }
+    lightsTog(){
+        if(this.lights==="OFF"){
+            this.lights = "ON"
+            return this.lights
+        }else{
+            this.lights = "OFF"
+            return this.lights
+    }}
     sigRight(){
         this.signal = "right"
         return this.signal
@@ -48,13 +48,16 @@ var myCar = new Car()
 var myTesla = new Tesla()
 var myToyota = new Toyota()
 // myTesla.sigLeft()
-// myTesla.lightsOn()
+myTesla.lightsTog()
 // console.log(myCar);
-// console.log(myTesla);
+console.log(myTesla);
 // console.log(myToyota);
 // console.log(myTesla.lightsOn())
 // console.log(myTesla.lightsOff())
 // console.log(myTesla.sigLeft());
+myTesla.lightsTog()
+// console.log(myCar);
+console.log(myTesla);
 
 
 ///// do next!!!!!!!! speed  of car
