1ea94db7ab19acfc10436f6e128959b250e4d04d
Travi5JAB <travis841235@gmail.com>
2019-04-29 14:19:26 -0700

    class assignments

--- class_assignments/class_inheritance.js
+++ class_assignments/class_inheritance.js
@@ -1,11 +1,14 @@
 class Car{
-    constructor(speed=0){
+    constructor(){
         this.model = "car"
         this.wheels = 4
         this.modelYear = 1992
         this.lights = "OFF"
         this.signal = "OFF"
-        this. speed = speed
+        this.speed = 0.0000000000000000000000000000001
+        this.miles = 0
+        this.lap = 0
+        this.time = 0
     }
     lightsTog(){
         if(this.lights==="OFF"){
@@ -28,53 +31,71 @@ class Car{
     }
     accelerate(speedUP){
         this.speed =  this.speed + speedUP
-        return this.speed
+        if(this.speed>200)
+            this.speed = 0
+            return this.speed
     }
     break(speedDown){
         this.speed = this.speed - speedDown
-        return this.speed
+        if(this.speed<0)
+            this.speed = 0
+            return this.speed
     }
+    miletracker(travel){
+        this.miles = this.miles + travel
+        this.lap = this.miles/2
+        this.time = this.lap/this.speed
+        return this.miles
+    }
+    // lapcounter(){
+    //     this.lap = this.miles*2
+    //     return this.lap
+//     }
 }
 class Tesla extends Car{
-    constructor(speed){
-        super(speed)
+    constructor(){
+        super()
         this.modelYear = 2019
         this.model = "Tesla"
     }
 }
 class Tata extends Car{
-    constructor(speed){
-        super(speed)
+    constructor(){
+        super()
         this.modelYear = 1984
         this.model = "Tata"
     }
 }
 class Toyota extends Car{
-    constructor(speed){
-        super(speed)
+    constructor(){
+        super()
         this.modelYear = 2000
         this.model = "Toyota"
+
     }
 }
 class Tesla2 extends Car{
-        constructor(speed){
-            super(speed)
+        constructor(){
+            super()
             this.modelYear = 2017
             this.model = "Tesla"
         }
     }
 class Tata2 extends Car{
-        constructor(speed){
-            super(speed)
+        constructor(){
+            super()
+            this.wheels = 3
             this.modelYear = 1974
             this.model = "Tata"
+            this.speed = 0
         }
     }
 class Toyota2 extends Car{
-        constructor(speed){
-            super(speed)
+        constructor(){
+            super()
             this.modelYear = 1980
             this.model = "Toyota"
+
         }
 }
 
@@ -99,21 +120,25 @@ return newArr= arr.map(value => value.model)
 }
 
 
-console.log(moC(myGarage),yoC(myGarage));
+// myTata2.accelerate(10)
+// console.log(moC(myGarage),yoC(myGarage));
+myToyota.accelerate(7)
+myToyota.miletracker(500)
+
 // console.log(myGarage);
-// myTesla.accelerate(10)
+// myTesla.accelerate(201)
 // myTesla.break(7)
 // myTata.accelerate(2)
 // myTata.break(1.25)
-// myToyota.accelerate(7)
+
 // myToyota.break(5)
 // console.log(myToyota.modelYear);
 // myTesla.sigLeft()
 // myTesla.lightsTog()
 // console.log(myCar);
 // console.log(myTesla);
-// console.log(myToyota);
-// console.log(myTata);
+console.log(myToyota);
+// console.log(myTata2);
 // console.log(myTesla.lightsOn())
 // console.log(myTesla.lightsOff())
 // console.log(myTesla.sigLeft());
